12a090f92264192d9db5a93cb0ff0f80d138d439
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-04-13 14:33:08 +0700

    deklarasi variabel

--- LinearSearch_130/LinearSearch_130.cpp
+++ LinearSearch_130/LinearSearch_130.cpp
@@ -2,8 +2,12 @@
 #include <iostream>
 using namespace std;
 
+int arr[20]; // Array to be searched
+int n; // number of elements in the array
+int i; // index of array elements
 
- 
+
+
 int main()
 {
     
