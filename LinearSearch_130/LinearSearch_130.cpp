abb0feac2e3c41d2f9f50068c6d1349a0d48bd3b
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-04-13 14:41:15 +0700

    input banyaknya array pada pembuatan void input

--- LinearSearch_130/LinearSearch_130.cpp
+++ LinearSearch_130/LinearSearch_130.cpp
@@ -7,6 +7,18 @@ int n; // number of elements in the array
 int i; // index of array elements
 
 
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+    }
+}
 
 int main()
 {
